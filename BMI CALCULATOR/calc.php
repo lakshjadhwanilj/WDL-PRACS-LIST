631388808cc558f16868fa39858f885eee356e28
Laksh Jadhwani <47735648+lakshjadhwanilj@users.noreply.github.com>
2019-10-27 16:30:00 +0530

    Delete calc.php

--- BMI CALCULATOR/calc.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $weight = $_POST['weight'];
-    $height = $_POST['height'];
-    $soln = $weight/($height*$height);
-    session_start();
-    $_SESSION['SOLN'] = $soln;
-    header("location: index.php");
-?>
