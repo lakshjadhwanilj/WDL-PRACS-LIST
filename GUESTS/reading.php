1e0351cc3b207bea09561acadf3c34e9047d1215
Laksh Jadhwani <47735648+lakshjadhwanilj@users.noreply.github.com>
2019-10-28 12:29:35 +0530

    Add files via upload

---
+++ GUESTS/reading.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>CRUD FUNCTIONS | READ</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div class="container">
+            
+            <table cellspacing="10px" cellpadding="5px"  style="border:1px solid black;" border="1">
+                <tr>                    
+                    <th>
+                        Name
+                    </th>
+                    <th>
+                        Mobile
+                    </th>
+                    <th>
+                        Address
+                    </th>
+                    <th>
+                        Gender
+                    </th>
+                </tr>
+                
+                <?php
+                    include_once 'include/include.php';
+                    $sql = "SELECT * FROM guestdetails;";
+                    $result = mysqli_query($conn, $sql);
+                    $resultCheck = mysqli_num_rows($result);
+                    if($resultCheck > 0) {
+                        while($row = mysqli_fetch_assoc($result)) {                            
+                            $name = $row['name'];
+                            $mobile = $row['mobile'];
+                            $address = $row['address'];
+                            $gender = $row['gender'];
+
+                            echo '<tr>                                        
+                                        <td>' . 
+                                            $name . '
+                                        </td>
+                                        <td>' . 
+                                            $mobile . '
+                                        </td>
+                                        <td>' . 
+                                            $address . '
+                                        </td>
+                                        <td>' . 
+                                            $gender . '
+                                        </td>
+                                </tr>';
+                        }
+                    }
+                ?>                
+            </table>
+        </div>
+    </body>
+</html>
